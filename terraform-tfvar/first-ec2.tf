9695e9625d45de0771f07ca270c344cf8d32b9d8
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2025-08-30 18:20:30 +0530

    count index

--- terraform-tfvar/first-ec2.tf
+++ terraform-tfvar/first-ec2.tf
@@ -5,6 +5,23 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
+  count =3
+  tags = {
+    Name = "MyFirstEC2-${count.index}"
+  }
 }
+variable "name"{
+  type = list
+  default = ["ec2-1","ec2-2","ec2-3"]
+}
+resource "aws_instance" "myec22" {
+  ami           = var.AMI_ID
+  instance_type = var.INSTANCE_TYPE
+  count =3
+  tags = {
+    Name = var.name[count.index]
+  }
+}
+
 
 
